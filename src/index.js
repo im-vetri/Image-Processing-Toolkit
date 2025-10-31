de07308e36376e238b6bcf0454983fdbda60f5c6
Vetri <vetri@github.com>
2025-10-31 18:18:13 +0530

    chore: restore source files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Image Processing Toolkit
+ * Advanced image manipulation and analysis suite
+ */
+
+export { ImageLoader } from './core/imageLoader.js';
+export { ColorSpace } from './core/colorSpace.js';
+export { BasicFilters } from './filters/basicFilters.js';
+export { EdgeDetection } from './filters/edgeDetection.js';
+export { Transform } from './filters/transform.js';
+export { HistogramEqualization } from './filters/histogramEqualization.js';
+export { QualityMetrics } from './filters/qualityMetrics.js';
+export { BatchProcessor } from './filters/batchProcessor.js';
